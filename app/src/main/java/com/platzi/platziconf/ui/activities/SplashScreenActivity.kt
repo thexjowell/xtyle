4f5a53951268076c4cff23e92ba7fa28227b17d9
thexjowell <ovando.arce.ariel.p@gmail.com>
2021-02-25 17:15:33 -0700

    Initial commit

---
+++ app/src/main/java/com/platzi/platziconf/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.platzi.platziconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.platzi.platziconf.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
